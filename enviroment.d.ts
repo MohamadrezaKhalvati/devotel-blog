4decabffa1f10d968ccdd57ba7989a81629a6a64
mohamad reza <mohamadrezakhf@gmail.com>
2024-12-19 11:55:04 +0330

    fix env config

--- enviroment.d.ts
+++ enviroment.d.ts
@@ -1,29 +1,13 @@
 export interface Env {
     APP_PORT: string
     NODE_ENV: 'DEVELOPMENT' | 'PRODUCTION'
+    APP_PORT: string
     POSTGRES_USER: string
+    POSTGRES_DB: string
     POSTGRES_PASSWORD: string
     POSTGRES_PORT: string
     POSTGRES_HOST: string
-    POSTGRES_DB: string
-    JWT_SECRET: string
-    ADMIN_USERNAME: string
-    ADMIN_PASSWORD: string
-    JWT_EXPIRE_TIME: string
-    SECRET_KEY: string
-    IV: string
-    FRONTEND_URL: string
-    API_URL: string
-    SMS_USERNAME: string
-    SMS_PASSWORD: string
-    SMS_NUMBER: string
-
-    ZARINPAL_MERCHANT_ID: string
-    ZIBAL_TOKEN: string
-    CALL_BACK_URL: string
-    KAVENEGAR_API_KEY: string
-    MELI_PAYAMK_API: string
-    SEND_SMS: 'YES' | 'NO'
+    APP_URL: string
 }
 
 declare global {
@@ -32,5 +16,4 @@ declare global {
     }
 }
 
-export { }
-
+export {}
